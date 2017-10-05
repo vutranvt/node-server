1889cae3cade6b31b98d95b18ce1c5019b7d1b52
vutranvt <vutranvt@gmail.com>
2017-10-05 09:20:00 +0700

    change code

--- server.js
+++ server.js
@@ -48,13 +48,13 @@ var authorizeSubscribe = function (client, topic, callback) {
 
 //here we start mosca
 var server = new mosca.Server(settings);
-server.attachHttpServer(httpServer);
+// server.attachHttpServer(httpServer);
 
 /*httpServer.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })*/
 
-httpServer.listen(9000);
+// httpServer.listen(9000);
 server.on('ready', setup);
  
 // httpServer.use('/', index);    
@@ -100,53 +100,64 @@ clientInfo collection = {
 server.on('clientConnected', function(client) {     
   	console.log('client connected', client.id);
 
-    mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
-        if (err) throw err;
-
-        var clientInfo = db.collection('clientInfo');
-        var infoData = {
-            clientState: "connected",
-            session: { 
-                startTime: Date(),
-                endTime: ""
+      mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
+        if (err){
+            console.log(err.name); 
+            console.log(err.message); 
+            //throw err;
+            // throw new Error(err.message);
+        } 
+        else {
+            var clientInfo = db.collection('clientInfo');
+            var infoData = {
+                clientState: "connected",
+                session: { 
+                    startTime: Date(),
+                    endTime: ""
+                }
             }
-        }
-
-        var newInfoData = {
-            clientId: client.id,    // mqtt client ID
-            dateInit: Date(),           // the first time device connect to server
-            clientState: "connected",   // state of client
-            session: {              // sesstion interval
-                startTime: Date(),  
-                endTime: ""
+    
+            var newInfoData = {
+                clientId: client.id,    // mqtt client ID
+                dateInit: Date(),           // the first time device connect to server
+                clientState: "connected",   // state of client
+                session: {              // sesstion interval
+                    startTime: Date(),  
+                    endTime: ""
+                }
             }
+    
+            // if found 'clientId' in mongodb: update info for client
+            // if not found 'clientId' in mongodb: insert new client 
+            clientInfo.find({clientId: client.id}).count(function(err, count){
+                if (err) {                    
+                    console.log(err);
+                    // console.log(err.name);
+                    // console.log(err.message);
+                    // throw err
+                }
+                else if (count==0){         // not found: update info for client 
+                    console.log('count:', count);
+                    clientInfo.insert(
+                        newInfoData,
+                        function (err,res) {
+                            if (err) console.log(err); 
+                            console.log('client connected updateOne-mongodb', res.modifiedCount);
+                            db.close();
+                    });
+                } else if (count==1) {      // found: insert new client
+                    console.log('count:', count);
+                    clientInfo.updateOne(
+                        { clientId: client.id}, 
+                        { $set: infoData},  
+                        function (err,res) {
+                            if (err)console.log(err);
+                            console.log('client connected updateOne-mongodb', res.modifiedCount);
+                            db.close();
+                    });
+                }                
+            });
         }
-
-        // if found 'clientId' in mongodb: update info for client
-        // if not found 'clientId' in mongodb: insert new client 
-        clientInfo.find({clientId: client.id}).count(function(err, count){
-            if (err) throw err
-            else if (count==0){         // not found: update info for client 
-                console.log('count:', count);
-                clientInfo.insert(
-                    newInfoData,
-                    function (err,res) {
-                        if (err) throw err;
-                        console.log('client connected updateOne-mongodb', res.modifiedCount);
-                        db.close();
-                });
-            } else if (count==1) {      // found: insert new client
-                console.log('count:', count);
-                clientInfo.updateOne(
-                    { clientId: client.id}, 
-                    { $set: infoData},  
-                    function (err,res) {
-                        if (err) throw err;
-                        console.log('client connected updateOne-mongodb', res.modifiedCount);
-                        db.close();
-                });
-            }                
-        });
     });
 });
  
@@ -155,10 +166,13 @@ server.on('published', function(packet, client) {
   	console.log('Published : ', JSON.stringify(packet.payload));
 
     mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
-        if (err) throw err;
-
+        if (err) {
+            // throw err;
+            console.log(err.name);
+            console.log(err.message);
+        }
         // insert data when client connected to server 
-        if(client){
+        else if(client){
             var clientInfo = db.collection('clientInfo');
             var infoData =  {
                 topic: packet.topic,
@@ -169,47 +183,48 @@ server.on('published', function(packet, client) {
             // if not found: insert new "publishers" for database ('clientInfo' collection)
             clientInfo.find({clientId: client.id, publishers: {$elemMatch: {topic: packet.topic}}})
             .count(function(err, count) {
-                if (err) throw err
-                var clientData = db.collection('clientData');
-                var publishData = {
-                    clientId: client.id,
-                    topic: packet.topic,
-                    value: packet.payload,
-                    timestamp: Date()
-                }
-                // insert publish data to "clientData" collection
-                clientData.insertOne(
-                    publishData, 
-                    function (err, res) {
-                        if (err) throw err;
-                        else {
-                            // not found: insert new "publishers"  (insert object in array)
-                            if (count==0) {
-                                console.log('count:', count);
-                                clientInfo.update(
-                                    { clientId: client.id}, 
-                                    { $addToSet: { publishers: infoData}}, 
-                                    function (err,res) {
-                                        if (err) throw err;
-                                        console.log('publish info update:');
-                                        db.close();
-                                });
-                            } 
-                            // found: update "publishers.state" 
-                            else if (count==1) {
-                                console.log('count:', count);
-                                clientInfo.update(
-                                    { clientId: client.id, "publishers.topic": packet.topic},
-                                    { $set: {"publishers.$.state": infoData.state}},
-                                    function(err, res) {
-                                console.log('count:', count);
-                                        if (err) throw err;
-                                        console.log('publish info insert :');
-                                        db.close();
-                                })
+                if (err) console.log(err);
+                else{
+                    var clientData = db.collection('clientData');
+                    var publishData = {
+                        clientId: client.id,
+                        topic: packet.topic,
+                        value: packet.payload,
+                        timestamp: Date()
+                    }
+                    // insert publish data to "clientData" collection
+                    clientData.insertOne(
+                        publishData, 
+                        function (err, res) {
+                            if (err) console.log(err);
+                            else {
+                                // not found: insert new "publishers"  (insert object in array)
+                                if (count==0) {
+                                    console.log('count:', count);
+                                    clientInfo.update(
+                                        { clientId: client.id}, 
+                                        { $addToSet: { publishers: infoData}}, 
+                                        function (err,res) {
+                                            if (err) console.log(err);
+                                            console.log('publish info update:');
+                                            db.close();
+                                    });
+                                } 
+                                // found: update "publishers.state" 
+                                else if (count==1) {
+                                    console.log('count:', count);
+                                    clientInfo.update(
+                                        { clientId: client.id, "publishers.topic": packet.topic},
+                                        { $set: {"publishers.$.state": infoData.state}},
+                                        function(err, res) {
+                                            if (err) console.log(err);
+                                            console.log('publish info insert :');
+                                            db.close();
+                                    })
+                                }
                             }
-                        }
-                });
+                    });
+                }
             });
         }
     });
@@ -221,46 +236,47 @@ server.on('subscribed', function(topic, client) {
 
     // add "clienId", "status"
   	mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
-  	    if (err) throw err;
+  	    if (err){
+            console.log(err.name);
+            console.log(err.message);
+        } else {
 
-  	    var clientInfo = db.collection('clientInfo');
-
-  	    var	data =  {
-            topic: topic,
-            state: "on"
-        }
-        
-        clientInfo.find({clientId: client.id, subscribers: {$elemMatch: {topic: topic}}}).count(function(err, count) {
-            if (err) throw err
-            // not found: insert new "subscribers" (insert object in array)
-            else if (count==0) {
-                console.log('count:', count);
-                clientInfo.update(
-                  { clientId: client.id}, 
-                  { $addToSet: { subscribers: data}}, 
-                  function (err,res) {
-                      if (err) throw err;
-                      // console.log('Subscribe mongodb:', res);
-                      db.close();
-                });
+            var clientInfo = db.collection('clientInfo');
+    
+            var	data =  {
+                topic: topic,
+                state: "on"
             }
-            // found: update "subscribers.state" 
-            else if (count==1) {
-                console.log('count:', count);
-                clientInfo.update(
-                    { clientId: client.id, "subscribers.topic": topic},
-                    { $set: {"subscribers.$.state": data.state}},
-                    function(err, res) {
-                        if (err) throw err;
+          
+            clientInfo.find({clientId: client.id, subscribers: {$elemMatch: {topic: topic}}}).count(function(err, count) {
+                if (err) console.log(err);
+                // not found: insert new "subscribers" (insert object in array)
+                else if (count==0) {
+                    console.log('count:', count);
+                    clientInfo.update(
+                    { clientId: client.id}, 
+                    { $addToSet: { subscribers: data}}, 
+                    function (err,res) {
+                        if (err) console.log(err);
                         // console.log('Subscribe mongodb:', res);
                         db.close();
-                })
-            }
-
-        });
-
+                    });
+                }
+                // found: update "subscribers.state" 
+                else if (count==1) {
+                    console.log('count:', count);
+                    clientInfo.update(
+                        { clientId: client.id, "subscribers.topic": topic},
+                        { $set: {"subscribers.$.state": data.state}},
+                        function(err, res) {
+                            if (err) console.log(err);
+                            // console.log('Subscribe mongodb:', res);
+                            db.close();
+                    })
+                }
+            });
+        }
   	});
-
 });
  
 
@@ -269,21 +285,23 @@ server.on('unsubscribed', function(topic, client) {
   	console.log('unsubscribed : ', topic);
 
     mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
-        if (err) throw err;
-
-        var clientInfo = db.collection('clientInfo');
-            
-        var data = "off"
-        
-        // 
-        clientInfo.update(
-            { clientId: client.id, "subscribers.state": "on"}, 
-            { $set: {"subscribers.$.state": data}}, 
-            function (err,res) {
-                if (err) throw err;
-                console.log('unsubscribed success :', topic);
-                db.close();
-        });
+        if (err) {
+            console.log(err.name);
+            console.log(err.message);
+        }
+        else {
+            var clientInfo = db.collection('clientInfo');
+            var data = "off";
+            // 
+            clientInfo.update(
+                { clientId: client.id, "subscribers.state": "on"}, 
+                { $set: {"subscribers.$.state": data}}, 
+                function (err,res) {
+                    if (err) console.log(err);
+                    console.log('unsubscribed success :', topic);
+                    db.close();
+            });
+        }
     });
 });
  
@@ -299,34 +317,62 @@ add "status: disconnected" into collection: clientInfo
 server.on('clientDisconnected', function(client) {
   	console.log('clientDisconnected : ', client.id);
     mongoClient.connect('mongodb://127.0.0.1:27017/nthdb', function(err, db) {
-        if (err) throw err;
-
-        var clientInfo = db.collection('clientInfo');
+        if (err) {
+            console.log(err.name);
+            console.log(err.message);
+        } else {
+            var clientInfo = db.collection('clientInfo');
                  
-        // update "publishers.state" in  "clientInfo"
-        var cursor = clientInfo.find({ clientId: client.id, publishers: {$elemMatch: {state: "on"}}});
-        
-        cursor.forEach(function (doc) {
+            /* clientInfo.find({ clientId: client.id, publishers: {$elemMatch: {state: "on"}}})
+            .count(function(err, count) {
+            if(err) throw err
+            else if (count==0) {
+                var infoData = {
+                    clientState: "disconnected",
 
-            doc.session.endTime = Date();
-
-            doc.publishers.forEach(function (publisher) {
-                if (publisher.state == "on") {
-                    publisher.state="off";
                 }
-            });
-            // update: "disconnected" , "publishers.state"s
-            clientInfo.updateOne(
-                { clientId: client.id }, 
-                { $set: 
-                    { clientState: "disconnected", session: doc.session, publishers: doc.publishers}
-                },
-                function(err, res){
-                    if (err) throw err;
-                    console.log('clientDisconnected success');
-                    db.close();
-            });
+                clientInfo.updateOne()
+            } 
+            }); */
+            //clientInfo.find({clien})
             
-        });            
+            // update "publishers.state" in  "clientInfo"
+            var cursor = clientInfo.find({ clientId: client.id/* , publishers: {$elemMatch: {state: "on"}} */});
+            
+            cursor.forEach(function (doc) {
+
+                doc.session.endTime = Date();
+                if(doc.publishers) {
+                    doc.publishers.forEach(function (publisher) {
+                        if (publisher.state == "on") {
+                            publisher.state="off";
+                        }
+                    });
+                    // update: "disconnected" , "publishers.state"s
+                    clientInfo.updateOne(
+                        { clientId: client.id }, 
+                        { $set: 
+                            { clientState: "disconnected", session: doc.session, publishers: doc.publishers}
+                        },
+                        function(err, res){
+                            if (err) console.log(err);
+                            console.log('clientDisconnected success');
+                            db.close();
+                    });
+                } else {
+                    clientInfo.updateOne(
+                        { clientId: client.id},
+                        { $set:
+                            { clientState: "disconnected", session: doc.session}
+                        },  
+                        function(err, res){
+                            if (err) console.log(err);
+                            console.log('clientDisconnected success');
+                            db.close();
+                    });
+                }
+            });            
+        }
     });
 });
+
